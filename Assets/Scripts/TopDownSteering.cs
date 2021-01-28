8f34c0e79f72070e6958441c9c755cbe4ed28ab6
Maxwell Anselm <manselm@lutron.com>
2021-01-28 12:50:19 -0500

    steer player with input

--- Assets/Scripts/TopDownSteering.cs
+++ Assets/Scripts/TopDownSteering.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// steers a rigidbody towards a given direction
 public class TopDownSteering : MonoBehaviour
 {
 	public Vector2 GoalDirection; // direction you want to steer towards
 	public float SteeringStrength; // how hard we will try to steer towards the goal
 	public float Speed;
 
-	private Rigidbody _rigidbody;
-	private Vector2 _steeringDirection; // actual steering direction
+	Rigidbody _rigidbody;
+	Vector2 _steeringDirection; // actual steering direction
 
 	void Awake()
 	{
@@ -19,12 +20,10 @@ public class TopDownSteering : MonoBehaviour
 		_steeringDirection = Vector2.zero;
 	}
 
-    // Start is called before the first frame update
     void Start()
     {
     }
 
-    // Update is called once per frame
     void Update()
     {
 		_steeringDirection = NewSteering(_steeringDirection, GoalDirection, SteeringStrength);
@@ -35,11 +34,13 @@ public class TopDownSteering : MonoBehaviour
 		_rigidbody.AddForce(SteeringForce(_steeringDirection, Speed));
 	}
 
-	Vector3 SteeringForce(Vector3 newSteeringDirection, float speed) {
+	Vector3 SteeringForce(Vector3 newSteeringDirection, float speed)
+	{
 		return new Vector3(newSteeringDirection.x, 0, newSteeringDirection.y) * speed;
 	}
 
-	Vector2 NewSteering(Vector2 oldSteeringDirection, Vector2 newGoalDirection, float lerp) {
+	Vector2 NewSteering(Vector2 oldSteeringDirection, Vector2 newGoalDirection, float lerp)
+	{
 		Vector2 truncatedGoal = newGoalDirection;
 		if (truncatedGoal.sqrMagnitude > 1.0f) {
 			truncatedGoal = truncatedGoal.normalized;
