0f15abdae217621cf202430f7730b6951a801b12
Maxwell Anselm <manselm@lutron.com>
2021-01-28 14:50:03 -0500

    movements feels ok
    had to really hack it with drag/gravity. using physics materials was no
    good

---
+++ Assets/Scripts/InputSteering.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSteering : MonoBehaviour
+{
+	TopDownSteering _steering;
+
+	void Awake()
+	{
+		_steering = GetComponent<TopDownSteering>();
+		if (_steering == null) { Debug.Log("can't find TopDownSteering component", this); }
+	}
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+		_steering.GoalDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+}
--- Assets/Scripts/TopDownSteering.cs
+++ Assets/Scripts/TopDownSteering.cs
@@ -7,7 +7,8 @@ public class TopDownSteering : MonoBehaviour
 {
 	public Vector2 GoalDirection; // direction you want to steer towards
 	public float SteeringStrength; // how hard we will try to steer towards the goal
-	public float Speed;
+	public float Speed; // multiplier applied to movement
+	public GameObject Visuals; // object that will be rotated to match steering
 
 	Rigidbody _rigidbody;
 	Vector2 _steeringDirection; // actual steering direction
@@ -16,6 +17,8 @@ public class TopDownSteering : MonoBehaviour
 	{
 		_rigidbody = GetComponent<Rigidbody>();
 		if (_rigidbody == null) { Debug.Log("no Rigidbody component found", this); }
+
+		if (Visuals == null) { Debug.Log("you forgot to set the visuals for steering", this); }
 
 		_steeringDirection = Vector2.zero;
 	}
@@ -27,11 +30,17 @@ public class TopDownSteering : MonoBehaviour
     void Update()
     {
 		_steeringDirection = NewSteering(_steeringDirection, GoalDirection, SteeringStrength);
+		Visuals.transform.localEulerAngles = FacingEulerAngles(_steeringDirection);
     }
 
 	void FixedUpdate()
 	{
-		_rigidbody.AddForce(SteeringForce(_steeringDirection, Speed));
+		_rigidbody.AddForce(SteeringForce(_steeringDirection, Speed), ForceMode.VelocityChange);
+	}
+
+	Vector3 FacingEulerAngles(Vector2 newSteeringDirection)
+	{
+		return new Vector3(0, -Mathf.Atan2(_steeringDirection.y, _steeringDirection.x) * 180.0f / Mathf.PI, 0);
 	}
 
 	Vector3 SteeringForce(Vector3 newSteeringDirection, float speed)
@@ -41,6 +50,9 @@ public class TopDownSteering : MonoBehaviour
 
 	Vector2 NewSteering(Vector2 oldSteeringDirection, Vector2 newGoalDirection, float lerp)
 	{
+		// TODO: if the new goal flips direction, this lerps through 0, which doesn't look good
+		// it would be nicer to somehow lerp *around* 0 so you get a turn instead of an instantaneous change
+		// i think you want to separately lerp rotation and magnitude
 		Vector2 truncatedGoal = newGoalDirection;
 		if (truncatedGoal.sqrMagnitude > 1.0f) {
 			truncatedGoal = truncatedGoal.normalized;
