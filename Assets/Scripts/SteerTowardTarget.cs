71ddf478b029f688a005a48849042e968a496256
Maxwell Anselm <manselm@lutron.com>
2021-01-29 15:10:46 -0500

    add magic cannon ball

---
+++ Assets/Scripts/SteerTowardTarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteerTowardTarget : MonoBehaviour
+{
+	public GameObject Target;
+
+	TopDownSteering _steering;
+
+	void Awake()
+	{
+		_steering = GetComponent<TopDownSteering>();
+		if (_steering == null) { Debug.Log("no top down steering on steer toward target!", gameObject); }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Target != null)
+		{
+			var diff = Target.transform.position - transform.position;
+			_steering.GoalDirection = new Vector2(diff.x, diff.z);
+		}
+    }
+}
