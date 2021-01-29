a4236cdd18fa5ded57b3e97afdd660435d49e0d0
Maxwell Anselm <manselm@lutron.com>
2021-01-28 21:09:56 -0500

    add digging UI

--- Assets/Scripts/Diggable.cs
+++ Assets/Scripts/Diggable.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Diggable : MonoBehaviour
 {
 	public float TotalWork;
 
-	float _workRemaining;
+	float _workDone;
 	Digger _digger;
+	Canvas _digUI;
+	Image _progressImage;
 
 	void Awake()
 	{
-		_workRemaining = TotalWork;
+		_digUI = GetComponentInChildren<Canvas>();
+		if (_digUI == null) { Debug.Log("no canvas component in diggable children", gameObject); }
+
+		_progressImage = _digUI.GetComponentInChildren<Image>();
+		if (_progressImage == null) { Debug.Log("no image component in diggable canvas children", gameObject); }
+
+		_workDone = 0;
 		_digger = null;
 	}
 
@@ -22,6 +31,7 @@ public class Diggable : MonoBehaviour
 	public void StartDigging(Digger digger)
 	{
 		_digger = digger;
+		_digUI.enabled = true;
 	}
 
 	public void StopDigging()
@@ -33,12 +43,16 @@ public class Diggable : MonoBehaviour
     {
 		if (_digger != null)
 		{
-			_workRemaining -= _digger.DigSpeed * Time.deltaTime;
+			_workDone += _digger.DigSpeed * Time.deltaTime;
 
-			if (_workRemaining <= 0)
+			if (_workDone >= TotalWork)
 			{
 				// TODO: give digger some reward
 				Destroy(gameObject);
+			}
+			else
+			{
+				_progressImage.fillAmount = _workDone / TotalWork;
 			}
 		}
     }
@@ -57,5 +71,6 @@ public class Diggable : MonoBehaviour
 		if (digger == null) return;
 
 		digger.UnsetTarget(this);
+		_digUI.enabled = false;
 	}
 }
