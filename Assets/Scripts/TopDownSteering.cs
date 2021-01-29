7d05663112460bdf3e5ac5730d112fbb10a38c2e
Maxwell Anselm <manselm@lutron.com>
2021-01-28 19:53:55 -0500

    refactor how steering works

--- Assets/Scripts/TopDownSteering.cs
+++ Assets/Scripts/TopDownSteering.cs
@@ -12,7 +12,8 @@ public class TopDownSteering : MonoBehaviour
 	public GameObject Visuals; // object that will be rotated to match steering
 
 	Rigidbody _rigidbody;
-	Vector2 _steeringDirection; // actual steering direction
+	float _steeringDirectionR; // direction we are steering in radians
+	float _steeringSpeed; // how hard we are moving in steering direction
 
 	void Awake()
 	{
@@ -29,9 +30,16 @@ public class TopDownSteering : MonoBehaviour
 			}
 		}
 
-		if (Visuals == null) { Debug.Log("you forgot to set the visuals for steering", this); }
+		if (Visuals == null)
+		{
+			Debug.Log("you forgot to set the visuals for steering", this);
+		}
+		else
+		{
+			_steeringDirectionR = Angle.Radians(Visuals.transform.localEulerAngles.y);
+		}
 
-		_steeringDirection = Vector2.zero;
+		_steeringSpeed = 0;
 	}
 
     void Start()
@@ -40,43 +48,59 @@ public class TopDownSteering : MonoBehaviour
 
     void Update()
     {
-		_steeringDirection = NewSteering(_steeringDirection, GoalDirection, SteeringStrength);
-		Visuals.transform.localEulerAngles = FacingEulerAngles(_steeringDirection);
+		var (dir, sp) = NewSteering(_steeringDirectionR, _steeringSpeed, GoalDirection, SteeringStrength);
+		_steeringDirectionR = dir;
+		_steeringSpeed = sp;
+
+		if (_steeringSpeed > 0)
+		{
+			Visuals.transform.localEulerAngles = FacingEulerAngles(_steeringDirectionR);
+		}
     }
 
 	void FixedUpdate()
 	{
-		_rigidbody.AddForce(SteeringForce(_steeringDirection, Speed), ForceMode.VelocityChange);
+		_rigidbody.AddForce(SteeringForce(_steeringDirectionR, Speed * _steeringSpeed), ForceMode.VelocityChange);
 		_rigidbody.velocity = DragXZVelocity(XZDrag, _rigidbody.velocity);
 	}
 
 	// mimic unity's drag calculation on velocity, but only in the XZ plane
-	Vector3 DragXZVelocity(float drag, Vector3 currentVelocity)
+	static Vector3 DragXZVelocity(float drag, Vector3 currentVelocity)
 	{
 		float multiplier = 1.0f - drag * Time.fixedDeltaTime;
 		if (multiplier < 0.0f) multiplier = 0.0f;
 		return Vector3.Scale(currentVelocity, new Vector3(multiplier, 1.0f, multiplier));
 	}
 
-	Vector3 FacingEulerAngles(Vector2 newSteeringDirection)
+	static Vector3 FacingEulerAngles(float newSteeringDirectionR)
 	{
-		return new Vector3(0, -Mathf.Atan2(_steeringDirection.y, _steeringDirection.x) * 180.0f / Mathf.PI, 0);
+		return new Vector3(0, Angle.Degrees(newSteeringDirectionR), 0);
 	}
 
-	Vector3 SteeringForce(Vector3 newSteeringDirection, float speed)
+	static Vector3 SteeringForce(float newSteeringDirectionR, float speed)
 	{
-		return new Vector3(newSteeringDirection.x, 0, newSteeringDirection.y) * speed;
+		return new Vector3(Mathf.Cos(newSteeringDirectionR), 0, -Mathf.Sin(newSteeringDirectionR)) * speed;
 	}
 
-	Vector2 NewSteering(Vector2 oldSteeringDirection, Vector2 newGoalDirection, float lerp)
+	static (float newDirectionR, float newSpeed) NewSteering(float oldSteeringDirectionR, float oldSpeed, Vector2 newGoalDirection, float lerp)
 	{
-		// TODO: if the new goal flips direction, this lerps through 0, which doesn't look good
-		// it would be nicer to somehow lerp *around* 0 so you get a turn instead of an instantaneous change
-		// i think you want to separately lerp rotation and magnitude
+		// first cap goal at 1 magnitude
 		Vector2 truncatedGoal = newGoalDirection;
 		if (truncatedGoal.sqrMagnitude > 1.0f) {
 			truncatedGoal = truncatedGoal.normalized;
 		}
-		return Vector2.Lerp(oldSteeringDirection, truncatedGoal, lerp);
+
+		// convert that to angle, defaulting to current facing if 0
+		float goalRadians = oldSteeringDirectionR;
+		if (truncatedGoal.sqrMagnitude > 0)
+		{
+			goalRadians = -Angle.Radians(truncatedGoal);
+		}
+
+		// find difference between old facing and goal facing
+		float diffR = Angle.RadianDiff(oldSteeringDirectionR, goalRadians);
+
+		// lerp facing and magnitude separately
+		return (oldSteeringDirectionR + Mathf.Lerp(0, diffR, lerp), Mathf.Lerp(oldSpeed, truncatedGoal.magnitude, lerp));
 	}
 }
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+[Flags]
+public enum Layers
+{
+	None = 0,
+    Default = 1 << 0,
+    TransparentFX = 1 << 1,
+    IgnoreRaycast = 1 << 2,
+    Water = 1 << 4,
+	UI = 1 << 5,
+	World = 1 << 8,
+}
+
+public class Angle
+{
+	private Angle() {}
+
+	public static float Degrees(float radians)
+	{
+		return radians * 180.0f / Mathf.PI;
+	}
+
+	public static float Radians(float degrees)
+	{
+		return degrees * Mathf.PI / 180.0f;
+	}
+
+	public static float Degrees(Vector2 direction)
+	{
+		return Degrees(Radians(direction));
+	}
+
+	public static float Radians(Vector2 direction)
+	{
+		return Mathf.Atan2(direction.y, direction.x);
+	}
+
+	public static float DegreeDiff(float start, float end)
+	{
+		return Degrees(RadianDiff(Radians(start), Radians(end)));
+	}
+
+	public static float RadianDiff(float start, float end)
+	{
+		return Mathf.Atan2(Mathf.Sin(end-start), Mathf.Cos(end-start));
+	}
+}
